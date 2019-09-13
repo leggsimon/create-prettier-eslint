fae9c63b5970a51ebad600d193e4eca25725afc3
Simon Legg <leggsimon@gmail.com>
2019-09-13 20:17:06 +0200

    move config into file

--- cli.js
+++ cli.js
@@ -6,106 +6,37 @@ const { promisify } = require('util');
 const inquirer = require('inquirer');
 const prettier = require('prettier');
 const ora = require('ora');
-
+const { defaults, questions } = require('./config');
+const { installDev } = require('./install-dependencies');
 const writeFile = promisify(fs.writeFile);
-
-const defaultEslintOptions = {
-	parserOptions: {
-		ecmaVersion: 2017,
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
-	extends: ['prettier'],
-	plugins: ['prettier'],
-	rules: {
-		'prettier/prettier': ['error'],
-	},
-};
-
-const defaultPrettierOptions = {
-	semi: true,
-	trailingComma: 'all',
-	singleQuote: true,
-	useTabs: true,
-};
 
 const format = (filename, contents, prettierConfig) => {
 	const isJS = filename.endsWith('.js');
-	return prettier.format(
-		`${isJS ? 'module.exports = ' : ''}${JSON.stringify(contents)}`,
-		{
-			filepath: filename,
-			...prettierConfig,
-			fileWidth: 60, // force it not to be written on a single line
-		},
-	);
+	return prettier.format(`${isJS ? 'module.exports = ' : ''}${JSON.stringify(contents)}`, {
+		filepath: filename,
+		...prettierConfig,
+		fileWidth: 60, // force it not to be written on a single line
+	});
 };
 
 inquirer
-	.prompt([
-		{
-			type: 'list',
-			name: 'prettierOverrides.useTabs',
-			message: 'Do you use tabs or spaces?',
-			choices: [
-				{ name: 'Tabs', value: true },
-				{ name: 'Spaces', value: false },
-			],
-		},
-		{
-			type: 'list',
-			name: 'prettierOverrides.semi',
-			message: 'Print semicolons at the ends of statements?',
-			choices: [{ name: 'Yes', value: true }, { name: 'No', value: false }],
-		},
-		{
-			type: 'list',
-			name: 'filenames.eslint',
-			message: 'What type of eslint config file would you like?',
-			choices: [
-				'.eslintrc.js',
-				'.eslintrc.json',
-				{
-					name: '.eslintrc (JSON syntax)',
-					value: '.eslintrc',
-				},
-			],
-		},
-		{
-			type: 'list',
-			name: 'filenames.prettier',
-			message: 'What type of prettier config file would you like?',
-			choices: [
-				'.prettierrc.js',
-				'.prettierrc.config.js',
-				'.prettierrc.json',
-				{
-					name: '.prettierrc (JSON syntax)',
-					value: '.prettierrc',
-				},
-			],
-		},
-	])
+	.prompt(questions)
 	.then(({ prettierOverrides, filenames }) => {
 		const prettierConfig = {
-			...defaultPrettierOptions,
+			...defaults.prettier,
 			...prettierOverrides,
 		};
 
 		const createFile = (filename, contents) =>
 			writeFile(filename, format(filename, contents, prettierConfig));
 
-		const writeEslintConfig = ora.promise(
-			createFile(filenames.eslint, defaultEslintOptions),
-			{ text: 'Creating eslint file' },
-		);
+		const writeEslintConfig = ora.promise(createFile(filenames.eslint, defaults.eslint), {
+			text: 'Creating eslint file',
+		});
 
-		const writePrettierConfig = ora.promise(
-			createFile(filenames.prettier, prettierConfig),
-			{ text: 'Creating prettier file' },
-		);
+		const writePrettierConfig = ora.promise(createFile(filenames.prettier, prettierConfig), {
+			text: 'Creating prettier file',
+		});
 
 		return Promise.all([writeEslintConfig, writePrettierConfig]);
 	})
---
+++ config.js
@@ -0,0 +1,68 @@
+module.exports = {
+	defaults: {
+		eslint: {
+			parserOptions: {
+				ecmaVersion: 2017,
+				sourceType: 'module',
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+			extends: ['prettier'],
+			plugins: ['prettier'],
+			rules: {
+				'prettier/prettier': ['error'],
+			},
+		},
+		prettier: {
+			semi: true,
+			trailingComma: 'all',
+			singleQuote: true,
+			useTabs: true,
+		},
+	},
+	questions: [
+		{
+			type: 'list',
+			name: 'prettierOverrides.useTabs',
+			message: 'Do you use tabs or spaces?',
+			choices: [
+				{ name: 'Tabs', value: true },
+				{ name: 'Spaces', value: false },
+			],
+		},
+		{
+			type: 'list',
+			name: 'prettierOverrides.semi',
+			message: 'Print semicolons at the ends of statements?',
+			choices: [{ name: 'Yes', value: true }, { name: 'No', value: false }],
+		},
+		{
+			type: 'list',
+			name: 'filenames.eslint',
+			message: 'What type of eslint config file would you like?',
+			choices: [
+				'.eslintrc.js',
+				'.eslintrc.json',
+				{
+					name: '.eslintrc (JSON syntax)',
+					value: '.eslintrc',
+				},
+			],
+		},
+		{
+			type: 'list',
+			name: 'filenames.prettier',
+			message: 'What type of prettier config file would you like?',
+			choices: [
+				'.prettierrc.js',
+				'.prettierrc.config.js',
+				'.prettierrc.json',
+				{
+					name: '.prettierrc (JSON syntax)',
+					value: '.prettierrc',
+				},
+			],
+		},
+	],
+};
