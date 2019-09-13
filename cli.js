4e0ac4b504e76366c8a4682006317ed64ed4d13a
Simon Legg <leggsimon@gmail.com>
2019-09-13 16:22:20 +0200

    ask about tabs or spaces

--- cli.js
+++ cli.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-const fs = require('fs').promises;
-
+const fs = require('fs');
+const { promisify } = require('util');
+const inquirer = require('inquirer');
 const prettier = require('prettier');
+
+const writeFile = promisify(fs.writeFile);
 
 const defaultEslintOptions = {
 	parserOptions: {
@@ -26,23 +29,45 @@ const defaultPrettierOptions = {
 	useTabs: true,
 };
 
-const writeEslintConfig = fs.writeFile(
-	'.eslintrc.js',
-	prettier.format(`module.exports = ${JSON.stringify(defaultEslintOptions)}`, {
-		filepath: '.eslintrc.js',
-		...defaultPrettierOptions,
-	}),
-);
+inquirer
+	.prompt([
+		{
+			type: 'list',
+			name: 'prettier.useTabs',
+			message: 'Do you use tabs or spaces?',
+			choices: [
+				{ name: 'Tabs', value: true },
+				{ name: 'Spaces', value: false },
+			],
+		},
+	])
+	.then(answers => {
+		const prettierConfig = {
+			...defaultPrettierOptions,
+			...answers.prettier,
+		};
+		const writeEslintConfig = writeFile(
+			'.eslintrc.js',
+			prettier.format(
+				`module.exports = ${JSON.stringify(defaultEslintOptions)}`,
+				{
+					filepath: '.eslintrc.js',
+					...prettierConfig,
+				},
+			),
+		);
 
-const writePrettierConfig = fs.writeFile(
-	'.prettierrc',
-	prettier.format(JSON.stringify(defaultPrettierOptions), {
-		filepath: '.prettierrc',
-		...defaultPrettierOptions,
-		printWidth: 20,
-	}),
-);
+		const writePrettierConfig = writeFile(
+			'.prettierrc',
+			prettier.format(JSON.stringify(prettierConfig), {
+				filepath: '.prettierrc',
+				...prettierConfig,
+				printWidth: 20,
+			}),
+		);
 
-Promise.all([writeEslintConfig, writePrettierConfig]).then(() => {
-	console.log('\x1b[32m', 'Done!', '\x1b[0m');
-});
+		return Promise.all([writeEslintConfig, writePrettierConfig]);
+	})
+	.then(() => {
+		console.log('\x1b[32m', 'Done!', '\x1b[0m');
+	});
