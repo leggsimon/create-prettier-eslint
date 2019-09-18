5a17250fdd7bc1bd01797ca384f0455ce2a8a4d3
Simon Legg <leggsimon@gmail.com>
2019-09-18 13:17:28 +0200

    refactor to async await

--- cli.js
+++ cli.js
@@ -13,11 +13,14 @@ const configFileName = join(homedir(), '.prettiereslint.config.json');
 
 const format = (filename, contents, prettierConfig) => {
 	const isJS = filename.endsWith('.js');
-	return prettier.format(`${isJS ? 'module.exports = ' : ''}${JSON.stringify(contents)}`, {
-		filepath: filename,
-		...prettierConfig,
-		fileWidth: 60, // force it not to be written on a single line
-	});
+	return prettier.format(
+		`${isJS ? 'module.exports = ' : ''}${JSON.stringify(contents)}`,
+		{
+			filepath: filename,
+			...prettierConfig,
+			fileWidth: 60, // force it not to be written on a single line
+		},
+	);
 };
 
 async function readConfigOrQuestion() {
@@ -30,46 +33,61 @@ async function readConfigOrQuestion() {
 	}
 }
 
-readConfigOrQuestion()
-	.then(({ prettierOverrides, filenames, saveGlobalConfig }) => {
-		const prettierConfig = {
-			...defaults.prettier,
-			...prettierOverrides,
-		};
+const createFile = async (filename, contents, prettierConfig) => {
+	try {
+		await writeFile(filename, format(filename, contents, prettierConfig));
+		console.log('\x1b[32m', '✔', '\x1b[0m', `Created ${filename}`);
+	} catch (error) {
+		console.log('\x1b[31m', '✘', '\x1b[0m', `Error creating ${filename}\n`);
+		console.log(error);
+	}
+};
 
-		const createFile = async (filename, contents) => {
-			try {
-				await writeFile(filename, format(filename, contents, prettierConfig));
-				console.log('\x1b[32m', '✔', '\x1b[0m', `Created ${filename}`);
-			} catch (error) {
-				console.log('\x1b[31m', '✘', '\x1b[0m', `Error creating ${filename}\n`);
-				console.log(error);
-			}
-		};
+(async () => {
+	const {
+		prettierOverrides,
+		filenames,
+		saveGlobalConfig,
+	} = await readConfigOrQuestion();
 
-		const writeEslintConfig = createFile(filenames.eslint, defaults.eslint);
+	const prettierConfig = {
+		...defaults.prettier,
+		...prettierOverrides,
+	};
 
-		const writePrettierConfig = createFile(filenames.prettier, prettierConfig);
+	const writeEslintConfig = createFile(
+		filenames.eslint,
+		defaults.eslint,
+		prettierConfig,
+	);
 
-		const promises = [writeEslintConfig, writePrettierConfig];
+	const writePrettierConfig = createFile(
+		filenames.prettier,
+		prettierConfig,
+		prettierConfig,
+	);
 
-		if (saveGlobalConfig) {
-			promises.push(
-				createFile(configFileName, { prettierOverrides: prettierConfig, filenames }),
-			);
-		}
+	const promises = [writeEslintConfig, writePrettierConfig];
 
-		return Promise.all(promises);
-	})
-	.then(() => {
-		console.log('Installing dependencies…');
+	if (saveGlobalConfig) {
+		promises.push(
+			createFile(configFileName, {
+				prettierOverrides: prettierConfig,
+				filenames,
+			}),
+		);
+	}
 
-		const requiredDependencies = [
-			'eslint',
-			'prettier',
-			'eslint-config-prettier',
-			'eslint-plugin-prettier',
-		];
+	await Promise.all(promises);
 
-		return installDev(requiredDependencies);
-	});
+	console.log('Installing dependencies…');
+
+	const requiredDependencies = [
+		'eslint',
+		'prettier',
+		'eslint-config-prettier',
+		'eslint-plugin-prettier',
+	];
+
+	return await installDev(requiredDependencies);
+})();
